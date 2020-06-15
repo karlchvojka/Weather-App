a29d241d1cabb18c51e9c5ec472a52415c76f361
Karl Chvojka <karl.chvojka@protonmail.com>
2020-06-14 21:05:07 -0400

    Added sass, got weather api to pull correctly

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// Imports
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.scss';
+import './Styles/fonts.scss';
+
+// Component Imports
+
+// Helper Functions
+const fetchWeather = async (setWeather) => {
+  const result = await axios(
+    "http://api.openweathermap.org/data/2.5/onecall?lat=43.6532&lon=79.3832&appid=1b6a6865f38ddcf18f28b82e13ba62d0"
+  );
+  // set resource for reference purposes.
+  let resource = result;
+  console.log(resource)
+  // Set current User state
+  setWeather(resource);
+};
 
 function App() {
+  const [weather, setWeather] = useState({});
+
+  useEffect(() => {
+    fetchWeather(setWeather);
+  }, []);
+
   return (
+    Object.keys(weather).length > 0 ?
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Weather App</h1>
     </div>
-  );
+    :
+      <h2>Loading...</h2>
+    );
 }
 
 export default App;
