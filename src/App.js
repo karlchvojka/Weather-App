6c7d6e254a1be8ee0a0236b589ddb212ad7821e4
Karl Chvojka <karl.chvojka@protonmail.com>
2020-06-29 23:18:00 -0400

    Built html structure for current day

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ const fetchWeather = async (setWeather, url) => {
 // fetchWeather fetches data from the API and saves it into state
 
 function App() {
+  // Variables
   const [url, setUrl] = useState('');
   const [weather, setWeather] = useState({});
 
@@ -39,12 +40,70 @@ function App() {
       console.log('no url')
   }, [url]);
 
-  console.log(weather)
+  console.log('we', weather);
   return (
     Object.keys(weather).length > 0 ?
     <div className="App">
-      <h1>Weather App</h1>
-      <p>{weather.lat}</p>
+      <header>
+        <h1>Weather App</h1>
+      </header>
+      <main>
+        <div className="weatherAppWrap">
+          <div className="currentWeather">
+            <div className="currentImg">
+              <img
+                src={`http://openweathermap.org/img/wn/${weather.current.weather[0].icon}@4x.png`}
+                />
+                </div>
+            <div className="currentLeft">
+              <div className="currentTitle">
+                <h2>
+                  <Moment
+                    format="MMM Do YYYY"
+                    unix
+                    >
+                      {weather.current.dt}
+                  </Moment>
+                </h2>
+              </div>
+              <div className="internal">
+                <div className="internal_left">
+                  <p>Current: {weather.current.temp}</p>
+                  <p>Feels Like: {weather.current.feels_like}</p>
+                  <p>Conditions: {weather.current.weather[0].description}</p>
+                  </div>
+                <div className="internal_right">
+                  <p>
+                    Humidity: {weather.current.humidity}%
+                    </p>
+                  <p>
+                    Sunrise:
+                    <Moment
+                      format="hh:mm A"
+                      unix
+                      >
+                        {weather.current.sunrise}
+                      </Moment>
+                    </p>
+                  <p>
+                    Sunset:
+                      <Moment
+                        format="hh:mm A"
+                        unix
+                        >
+                          {weather.current.sunset}
+                        </Moment>
+                    </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="dailyWrap">
+            <div>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
     :
       <h2>Loading...</h2>
